032cdf504d39a064431b45e158297d0f614f5c0d
여민호 <ymh254@gmail.com>
2020-10-04 22:56:41 +0900

    add WordRelay.js

---
+++ WordRelay.js
@@ -0,0 +1,11 @@
+//끝말잇기
+var word = '제시어';
+while(true){
+    var answer = prompt(word);
+    if(word[word.length-1]===answer[0]){
+        alert('딩동댕');
+        word=answer;
+    }else{
+        alert('땡');
+    }
+}
