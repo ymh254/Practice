980058827ca6bc7ecbc3b1589798b1efa273936d
여민호 <ymh254@gmail.com>
2020-10-06 23:58:25 +0900

    add Multiplication.js

---
+++ Multplication.js
@@ -0,0 +1,17 @@
+while (true) {
+    var number1 = Math.floor(Math.random() * 9) + 1
+    var number2 = Math.floor(Math.random() * 9) + 1
+    var result = number1 * number2
+    var a = true;
+    while (a) {
+        var answer = prompt(String(number1)+'x'+String(number2)+'=?')
+        //prompt에서 취소를 누르면 null이 값으로 들어감.
+        //Number(null) = 0 이므로 false로 처리됨.
+        if (result === Number(result)) {
+            alert('Correct!')
+            a=false;
+        }else{
+            alert('Incorrect!')
+        }
+    }
+}
