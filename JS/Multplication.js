ddcf21d94e0597dcf52bd2e78ddcec7ddf91ebeb
여민호 <ymh254@gmail.com>
2020-10-16 23:57:54 +0900

    add React repository

--- JS/Multplication.js
+++ JS/Multplication.js
@@ -7,7 +7,7 @@ while (true) {
         var answer = prompt(String(number1)+'x'+String(number2)+'=?')
         //prompt에서 취소를 누르면 null이 값으로 들어감.
         //Number(null) = 0 이므로 false로 처리됨.
-        if (result === Number(result)) {
+        if (result === Number(answer)) {
             alert('Correct!')
             a=false;
         }else{
