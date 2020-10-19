8498ed2a03f91e690bc085948f5b2b945a3a9f7d
여민호 <ymh254@gmail.com>
2020-10-19 23:59:23 +0900

    modify WordRelay.js

--- JS/WordRelay.js
+++ JS/WordRelay.js
@@ -9,3 +9,16 @@ while(true){
         alert('땡');
     }
 }
+
+//HTML로 만들기!!
+var 바디 = document.body;
+var 단어 = document.createElement('div');
+단어.textContent = '제시어';
+document.body.append(단어);
+var 입력창 = document.createElement('input');
+document.body.append(입력창);
+var 버튼 = document.createElement('button');
+버튼.textContent = '입력';
+document.body.append('버튼');
+var 결과창 = document.createElement('div');
+document.body.append(결과창);
