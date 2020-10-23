3619add458f4cc8db1899e16b4663945d8e9cc66
ymh254 <ymh254@gmail.com>
2020-10-23 23:57:49 +0900

    modify hello-react

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import Counter from './Counter';
-import Stopwatch from "./Stopwatch"
-import Countdown from "./Countdown"
 import Navbar from './Navbar/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -20,10 +18,6 @@ function App() {
                   <Route path='/about' component={About} />
                 </Switch>
             </Router>
-          <div className = "Timers">
-            <Stopwatch />
-            <Countdown />
-          </div>
         </div>
       </>
     );
--- React/hello-react/src/pages/About.js
+++ React/hello-react/src/pages/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import Stopwatch from "../Stopwatch"
+import Countdown from "../Countdown"
+
 function About() {
     return (
         <div className='about'>
             <h1>About</h1>
+            <div className = "Timers">
+                <Stopwatch />
+                <Countdown />
+            </div>
         </div>
+        
     );
 }
 
