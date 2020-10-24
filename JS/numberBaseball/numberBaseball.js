33191e6af5ddbf5e316a410e61f1db35abd192e9
ymh254 <ymh254@gmail.com>
2020-10-25 00:51:12 +0900

    modify numberBaseball.js

--- JS/numberBaseball/numberBaseball.js
+++ JS/numberBaseball/numberBaseball.js
@@ -23,6 +23,8 @@ var 버튼 = document.createElement('button');
 버튼.textContent = '입력';
 폼.append(버튼);
 
+var 틀린횟수 = 0;
+
 폼.addEventListener('submit', function 비동기(이벤트){
     이벤트.preventDefault();
     var 답 = 입력창.value;
@@ -40,15 +42,29 @@ var 버튼 = document.createElement('button');
         var 답배열 = 답.split('');
         var 스트라이크 = 0;
         var 볼 = 0;
-        for (var i=0; i<3; i += 1) {
-            if (Number(답배열[i])===숫자배열[i]){ //같은자리인지 확인
-               스트라이크 += 1; 
-            } else if (숫자배열.indexOf(Number(답배열[i])) > -1){ //같은자리는 아니지만, 있는 숫자인지 확인
-                볼 += 1;
+        틀린횟수 +=1;
+        if (틀린횟수 > 10) {
+            결과.textContent = "10회 오답! 실패! 답은" + 숫자배열.join(',') + "였습니다..";
+            입력창.value = '';
+            입력창.focus();
+            숫자후보 = [1,2,3,4,5,6,7,8,9];
+            숫자배열 = [];
+            for (var i=0; i<4; i += 1) {
+                var 뽑은숫자 = 숫자후보.splice(Math.floor(Math.random()* (9-i)),1)[0];
+                숫자배열.push(뽑은숫자);
             }
+            틀린횟수 = 0;
+        } else {
+            for (var i=0; i<4; i += 1) {
+                if (Number(답배열[i])===숫자배열[i]){ //같은자리인지 확인
+                스트라이크 += 1; 
+                } else if (숫자배열.indexOf(Number(답배열[i])) > -1){ //같은자리는 아니지만, 있는 숫자인지 확인
+                    볼 += 1;
+                }
+            }
+            결과.textContent = 스트라이크 + '스트라이크' + 볼 + '볼 입니다.';
+            입력창.value = '';
+            입력창.focus();
         }
-        결과.textContent = 스트라이크 + '스트라이크' + 볼 + '볼 입니다.';
-        입력창.value = '';
-        입력창.focus();
     }
 })
