d438a273499e3e29ec2dacff0c86d94ea240bcde
ymh254 <ymh254@gmail.com>
2020-10-26 20:57:02 +0900

    add myPage

--- React/hello-react/src/pages/About.js
+++ React/hello-react/src/pages/About.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import './About.css'
 
-import Stopwatch from "../Stopwatch"
-import Countdown from "../Countdown"
+import Profile from './Profile.png'
+import MyCalendar from './MyCalendar'
 
-function About() {
+const About = () => {
     return (
-        <div className='about'>
-            <h1>About</h1>
-            <div className = "Timers">
-                <Stopwatch />
-                <Countdown />
+        <div>
+            <div class="ui grid">
+                <div class="row">
+                    <div class="six wide column" className="image">
+                        <img class="disabled medium ui image" src={Profile} />
+                    </div>
+                    <div class="ten wide column" className="profile" style={{ fontSize: "24px" }}>
+                        <p style={{ margin: 0 }}><strong>Profile</strong></p>
+                        <div class="ui segment" style={{ height: "11.5rem" }}>
+                            <p style={{ lineHeight: "25px" }}>
+                                <strong>이름</strong> : <br />
+                                <strong>Email</strong> : <br />
+                                <strong>관심분야</strong> : <br />
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="six wide column" className="todo">
+                        <div class="ui segment" style={{ height: "18rem" }}>
+                            <p style={{ fontSize: "18px" }}>
+                                <strong>Todo List</strong>
+                            </p>
+                            <p>
+                                오늘의 Todo를 작성하시오.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="ten wide column" className="calendar">
+                        <MyCalendar />
+                    </div>
+                </div>
             </div>
-        </div>
-        
+        </div >
     );
-}
+};
 
 export default About;
--- React/hello-react/src/pages/Home.js
+++ React/hello-react/src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Stopwatch from "../Stopwatch"
+import Countdown from "../Countdown"
+
 function Home() {
     return (
         <div className='home'>
             <h1>Home</h1>
+            <div className = "Timers">
+                <Stopwatch />
+                <Countdown />
+            </div>
         </div>
     );
 }
