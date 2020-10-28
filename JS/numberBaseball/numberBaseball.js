064f8a29e7fcefc00910142558e6e83d3233c2d6
ymh254 <ymh254@gmail.com>
2020-10-28 22:58:59 +0900

    modify numberBaseball.js

--- JS/numberBaseball/numberBaseball.js
+++ JS/numberBaseball/numberBaseball.js
@@ -1,14 +1,18 @@
 var 바디 = document.body;
 
-var 숫자후보 = [1,2,3,4,5,6,7,8,9];
+var 숫자후보;
+var 숫자배열;
 
-var 숫자배열 = [];
-
-for(var i=0; i<4; i += 1){
-    var 뽑은숫자 = 숫자후보.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
-    숫자배열.push(뽑은숫자);
+function 숫자뽑기() {
+    숫자후보 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    숫자배열 = [];
+    for (var i = 0; i < 4; i += 1) {
+        var 뽑은숫자 = 숫자후보.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        숫자배열.push(뽑은숫자);
+    }
 }
 
+숫자뽑기();
 console.log(숫자배열);
 
 var 결과 = document.createElement('h1');
@@ -17,48 +21,51 @@ var 폼 = document.createElement('form');
 document.body.append(폼);
 var 입력창 = document.createElement('input');
 폼.append(입력창);
-입력창.type='text';
-입력창.maxLength=4;
+입력창.type = 'text';
+입력창.maxLength = 4;
 var 버튼 = document.createElement('button');
 버튼.textContent = '입력';
 폼.append(버튼);
 
 var 틀린횟수 = 0;
 
-폼.addEventListener('submit', function 비동기(이벤트){
+폼.addEventListener('submit', function 비동기(이벤트) {
     이벤트.preventDefault();
     var 답 = 입력창.value;
     if (답 === 숫자배열.join('')) {
         결과.textContent = '홈런!';
         입력창.value = '';
         입력창.focus();
-        숫자후보 = [1,2,3,4,5,6,7,8,9];
-        숫자배열 = [];
-        for (var i=0; i<4; i += 1) {
-            var 뽑은숫자 = 숫자후보.splice(Math.floor(Math.random()* (9-i)),1)[0];
-            숫자배열.push(뽑은숫자);
-        }
-    }else {  //답이 틀리면
+        숫자뽑기();
+        // 숫자후보 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        // 숫자배열 = [];
+        // for (var i = 0; i < 4; i += 1) {
+        //     var 뽑은숫자 = 숫자후보.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        //     숫자배열.push(뽑은숫자);
+        // }
+        틀린횟수 = 0;
+    } else {  //답이 틀리면
         var 답배열 = 답.split('');
         var 스트라이크 = 0;
         var 볼 = 0;
-        틀린횟수 +=1;
+        틀린횟수 += 1;
         if (틀린횟수 > 10) {
             결과.textContent = "10회 오답! 실패! 답은" + 숫자배열.join(',') + "였습니다..";
             입력창.value = '';
             입력창.focus();
-            숫자후보 = [1,2,3,4,5,6,7,8,9];
-            숫자배열 = [];
-            for (var i=0; i<4; i += 1) {
-                var 뽑은숫자 = 숫자후보.splice(Math.floor(Math.random()* (9-i)),1)[0];
-                숫자배열.push(뽑은숫자);
-            }
+            숫자뽑기();
+            // 숫자후보 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            // 숫자배열 = [];
+            // for (var i = 0; i < 4; i += 1) {
+            //     var 뽑은숫자 = 숫자후보.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+            //     숫자배열.push(뽑은숫자);
+            // }
             틀린횟수 = 0;
         } else {
-            for (var i=0; i<4; i += 1) {
-                if (Number(답배열[i])===숫자배열[i]){ //같은자리인지 확인
-                스트라이크 += 1; 
-                } else if (숫자배열.indexOf(Number(답배열[i])) > -1){ //같은자리는 아니지만, 있는 숫자인지 확인
+            for (var i = 0; i < 4; i += 1) {
+                if (Number(답배열[i]) === 숫자배열[i]) { //같은자리인지 확인
+                    스트라이크 += 1;
+                } else if (숫자배열.indexOf(Number(답배열[i])) > -1) { //같은자리는 아니지만, 있는 숫자인지 확인
                     볼 += 1;
                 }
             }
