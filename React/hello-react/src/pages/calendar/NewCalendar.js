89577ce8d718cf827b41a4369779e160f8a34339
ymh254 <ymh254@gmail.com>
2020-11-02 23:47:36 +0900

    modify NewCalendar.js

--- React/hello-react/src/pages/calendar/NewCalendar.js
+++ React/hello-react/src/pages/calendar/NewCalendar.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction";
 import "./calendar.scss"
 
 export default class NewCalendar extends React.Component {
-
     render() {
+
         return (
             <div className="calendar_wrap">
 
                 <FullCalendar
                     defaultView="dayGridMonth"
-                    plugins={[dayGridPlugin]}
-                    events={[{ title: "출석완료", date: "2020-10-31" }]} />
+                    plugins={[dayGridPlugin, interactionPlugin]}
+                    events={[{ title: "출석완료", date: "2020-10-31" }]}
+                    dateClick={this.handleDateClick}
+                />
             </div>
 
         )
     }
-
+    handleDateClick = () => { // bind with an arrow function
+        alert("arg.dateStr")
+    }
 }
