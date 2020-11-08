fd94b915479d79daf217b0107dd7d8d9ee89f940
ymh254 <ymh254@gmail.com>
2020-11-08 23:59:36 +0900

    practice calendar API

--- React/hello-react/src/pages/calendar/NewCalendar.js
+++ React/hello-react/src/pages/calendar/NewCalendar.js
@@ -6,13 +6,20 @@ import "./calendar.scss"
 
 export default class NewCalendar extends React.Component {
     render() {
+        Date.prototype.yyyymmdd = function () {
+            var yyyy = this.getFullYear().toString();
+            var mm = (this.getMonth() + 1).toString();
+            var dd = this.getDate().toString();
+
+            return yyyy + "-" + (mm[1] ? mm : '0' + mm[0]) + "-" + (dd[1] ? dd : '0' + dd[0]);
+        }
 
         return (
             <div className="calendar_wrap">
                 <FullCalendar
                     defaultView="dayGridMonth"
                     plugins={[dayGridPlugin, interactionPlugin]}
-                    events={[{ title: "출석완료", date: "2020-10-31" }]}
+                    events={[{ title: "출석완료", date: "2020-11-08" }]}
                     dateClick={this.handleDateClick}
                     backgroundColor={'#378006'}
                 />
@@ -20,7 +27,9 @@ export default class NewCalendar extends React.Component {
 
         )
     }
-    handleDateClick = () => { // bind with an arrow function
-        alert("arg.dateStr")
+    handleDateClick = (arg) => { // bind with an arrow function
+        console.log(arg.dateStr)
+        // const tdate = arg.dateStr;
+        // this.props.setDate(tdate);
     }
 }
