5ca6a97bd2df732c71f7544574217695be7d483e
ymh254 <ymh254@gmail.com>
2020-12-02 23:54:04 +0900

    modify DateFunction

--- React/hello-react/src/pages/calendar/DateFunction.js
+++ React/hello-react/src/pages/calendar/DateFunction.js
@@ -1,4 +1,14 @@
+// export default Date.prototype.yyyymmdd = function () {
+//     const dateObj = new Date();
+//     return (dateObj.getUTCFullYear()) + "-" + (dateObj.getMonth() + 1) + "-" + (dateObj.getUTCDate());
+// }
+// 10일 미만시 mm형태가 아닌 m형태로 찍힘
+
 export default Date.prototype.yyyymmdd = function () {
-    const dateObj = new Date();
-    return (dateObj.getUTCFullYear()) + "-" + (dateObj.getMonth() + 1) + "-" + (dateObj.getUTCDate());
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (1 + date.getMonth())).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+
+    return year + "-" + month + "-" + day;
 }
