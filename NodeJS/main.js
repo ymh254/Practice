46ef92ed8f92c68aa64b7e398043d55d692d4d8f
ymh254 <ymh254@gmail.com>
2021-03-16 22:02:35 +0900

    modify files -add functions

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
     return `
     <!doctype html>
     <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
-        <a href="/create">create</a>
+        ${control}
         ${body}
     </body>
     </html>
@@ -39,19 +39,25 @@ var app = http.createServer(function (request, response) {
                 var title = 'Welcome'
                 var description = 'Hello, NodeJS!'
                 var list = templateList(files);
-                var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`)
+                var template = templateHTML(title, list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create</a>`
+                );
                 response.writeHead(200);
                 // response.end(fs.readFileSync(__dirname + url));
-                response.end(template)
+                response.end(template);
             })
 
 
         } else {
             fs.readdir('./data', function (err, files) {
-                var list = templateList(files)
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id;
-                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`)
+                    var list = templateList(files)
+                    var template = templateHTML(title, list,
+                        `<h2>${title}</h2>${description}`,
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                    );
                     response.writeHead(200);
                     // response.end(fs.readFileSync(__dirname + url));
                     response.end(template)
@@ -63,7 +69,7 @@ var app = http.createServer(function (request, response) {
             var title = 'WEB-create'
             var list = templateList(files);
             var template = templateHTML(title, list, `
-            <form action="http://localhost:3000/create_process" method="post">
+            <form action="/create_process" method="post">
                 <p><input type="text" name="title" placeholder="title"></p>
                 <p>
                     <textarea name="description" placeholder="description"></textarea>
@@ -72,7 +78,7 @@ var app = http.createServer(function (request, response) {
                     <input type="submit">
                 </p>
             </form>
-            `);
+            `, '');
             response.writeHead(200);
             response.end(template)
         });
@@ -88,6 +94,48 @@ var app = http.createServer(function (request, response) {
             fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
                 response.writeHead(302, { Location: `/?id=${title}` });
                 response.end();
+            });
+        });
+    } else if (pathname === '/update') {
+        fs.readdir('./data', function (err, files) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                var title = queryData.id;
+                var list = templateList(files)
+                var template = templateHTML(title, list,
+                    `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                    `,
+                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                );
+                response.writeHead(200);
+                // response.end(fs.readFileSync(__dirname + url));
+                response.end(template)
+            });
+        });
+    } else if (pathname === "/update_process") {
+        var body = '';
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function (err) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+                    response.writeHead(302, { Location: `/?id=${title}` });
+                    response.end();
+                });
             });
         });
     } else {
